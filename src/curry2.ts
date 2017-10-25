8bfbb64e9361e8a453fe43d0ac8d7b2ee7181f3c
Andre Staltz <andre@staltz.com>
2017-10-25 22:52:54 +0300

    Avoid shadowing parameter name, fix issue #13

--- src/curry2.ts
+++ src/curry2.ts
@@ -2,11 +2,11 @@ import { VNode } from 'snabbdom/vnode';
 import { Select, Selector } from './types';
 
 export function curry2 (select: Select): Selector {
-  return function selector (selector: string, vNode: VNode): any {
+  return function selector (sel: string, vNode: VNode): any {
     switch (arguments.length) {
       case 0: return select;
-      case 1: return (_vNode: VNode) => select(selector, _vNode);
-      default: return select(selector, vNode);
+      case 1: return (_vNode: VNode) => select(sel, _vNode);
+      default: return select(sel, vNode);
     }
   } as Selector;
 };
