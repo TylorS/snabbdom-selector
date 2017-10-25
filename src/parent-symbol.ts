b53b76bd3ab0e5e0c7fe2f4be1bd54ff92eb0860
Andre Staltz <andre@staltz.com>
2017-10-25 23:07:26 +0300

    Add a string-based fallback when Symbol is not available
    Fixes issue #22

--- src/parent-symbol.ts
+++ src/parent-symbol.ts
@@ -1,3 +1,23 @@
-const parentSymbol: symbol = Symbol('parent');
+declare var global: any;
+
+let root: any;
+if (typeof self !== 'undefined') {
+  root = self;
+} else if (typeof window !== 'undefined') {
+  root = window;
+} else if (typeof global !== 'undefined') {
+  root = global;
+} else {
+  root = Function('return this')();
+}
+
+const Symbol = root.Symbol;
+
+let parentSymbol: symbol;
+if (typeof Symbol === 'function') {
+  parentSymbol = Symbol('parent');
+} else {
+  parentSymbol = '@@snabbdom-selector-parent' as any;
+}
 
 export default parentSymbol;
